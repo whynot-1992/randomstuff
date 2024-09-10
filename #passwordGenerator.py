dca840a3810e89777081f2c2b2bc09d3836ba6d7
whynot-1992 <rvivaan896@gmail.com>
2024-09-10 16:13:47 +1000

    Update #passwordGenerator.py

--- #passwordGenerator.py
+++ #passwordGenerator.py
@@ -4,11 +4,14 @@ import random
 print("Password generator")
 print()
 print("Your password is...")
+print()
 
 def generatePassword():
-  wordList = "helloWorld", "iLoveCoding", "monkeyLover", "brainrotPerson", "godGamer"
-  numList = "984", "690", "420", "314", "469", "908"
-  generatedPassword = wordList[random.randint(0, 4)] + numList[random.randint(0, 5)]
-  return generatedPassword
+  wordList = ["helloWorld", "iLoveCoding", "monkeyLover", "brainrotPerson", "godGamer"]
+  numList = ["984", "690", "420", "314", "469", "908", "911"]
+  wordIndex = random.randint(0, 4)
+  numIndex = random.randint(0, 6)
+  generatedPassword = wordList[wordIndex] + numList[numIndex]
+  print(generatedPassword)
 
-generatedPassword()
+generatePassword()
