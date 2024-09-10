3ae2a049300240f017e2f715fbb6eeb2cf1863b5
whynot-1992 <rvivaan896@gmail.com>
2024-09-10 14:56:19 +1000

    Update #passwordGenerator.py

--- #passwordGenerator.py
+++ #passwordGenerator.py
@@ -1,1 +1,13 @@
 #passwordGenerator
+
+print("Password generator")
+print()
+print("Your password is...")
+
+def generatePassword():
+  wordList = "helloWorld", "iLoveCoding", "monkeyLover", "brainrotPerson", "godGamer"
+  numList = "984", "690", "420", "314", "469", "908"
+  generatedPassword = wordList[random.randint(0, 4)] + numList[random.randint(0, 5)]
+  return generatedPassword
+
+generatedPassword()
